9a34d4dae427896a60c2c87d8f34aa142147dc04
Zackysfn <zackyngoding@gmail.com>
2026-05-18 00:05:22 +0800

    Refactor mergeTwoLists with improved comments
    Updated comments and formatting in the mergeTwoLists function.

--- mergetwosortedlist.c
+++ mergetwosortedlist.c
@@ -1,16 +1,21 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+/*
+     Definition for singly-linked list.
+    
+      struct ListNode {
+         int val;
+          struct ListNode *next;
+      };
+*/
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+
+    //buat node dummy
     struct ListNode dummy;
     struct ListNode* curr = &dummy;
     dummy.next = NULL;
 
+
+    //lakukan perbandingan ke masing-masing head di tiap list selagi tidak ada list yang kosong
     while (list1 != NULL && list2 != NULL) {
         if (list1->val <= list2->val) {
             curr->next = list1;    
@@ -28,5 +33,5 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
         curr->next = list2;
     }
 
-    return dummy.next;
+    return dummy.next; //kembalikan alamat node yang ditunjuk dummy yang merupakan head dari sorted list dari 2 list yang sudah di merge tadi
 }
